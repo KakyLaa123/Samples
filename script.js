1ad73a7637a3e9f53d562904f4872c522737100b
Alexandr Kuchmin <kakylaa123@gmail.com>
2021-11-22 01:55:21 +0200

    fix

--- script.js
+++ script.js
@@ -23,8 +23,6 @@ const hideResults = document.getElementById('hide');
 
 let itsPossible = false;
 
-
-
 takeIt.addEventListener('click', (e) => {
     e.preventDefault();
     for(let i = 0; i < inputs.length - 1; i++){    
@@ -34,20 +32,19 @@ takeIt.addEventListener('click', (e) => {
             itsPossible = false;
         }
     }
-        if(itsPossible){
-            results.classList.remove('hidden');
-            ketle.textContent = (weight.value / ((height.value * height.value) / 10000)).toFixed(2);
-            if (ketle.textContent === 'NaN'){
-                ketle.textContent = 'Неверные данные';
-            }
-            excurtion.textContent = inhale.value - exhalation.value;
-            rufie.textContent = ((4 * (pulseCalm.value + (pulseCalm.value + 22) + (pulseCalm.value + 10)) - 200) / 10000000000).toFixed(2);
-            oap.textContent = (0.011 * pulseCalm.value + 0.014 * bp1.value + 0.008 * bp2.value + 0.014 * age.value + 0.009 * weight.value - 0.009 * height.value - 0.27).toFixed(2);
-            if (oap.textContent < 0){
-                oap.textContent = 'Неверные данные';
-            }
+    if(itsPossible){
+        results.classList.remove('hidden');
+        ketle.textContent = (weight.value / ((height.value * height.value) / 10000)).toFixed(2);
+        if (ketle.textContent === 'NaN'){
+        ketle.textContent = 'Неверные данные';
         }
-
+        excurtion.textContent = inhale.value - exhalation.value;
+        rufie.textContent = ((4 * (pulseCalm.value + (pulseCalm.value + 22) + (pulseCalm.value + 10)) - 200) / 10000000000).toFixed(2);
+        oap.textContent = (0.011 * pulseCalm.value + 0.014 * bp1.value + 0.008 * bp2.value + 0.014 * age.value + 0.009 * weight.value - 0.009 * height.value - 0.27).toFixed(2);
+        if (oap.textContent < 0){
+            oap.textContent = 'Неверные данные';
+        }
+    }
 });
 
 hideResults.addEventListener('click', () => {
